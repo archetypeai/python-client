ce889edb625bba89e76cf84074827bef2b9d0a86
Nick Gillian <nick@archetypeai.team>
2024-06-15 04:00:06 +0000

    Improved kwarg filtering.

--- src/archetypeai/api_client.py
+++ src/archetypeai/api_client.py
@@ -4,6 +4,7 @@ from requests_toolbelt import MultipartEncoder
 import os
 from typing import Dict, List, Tuple
 from pathlib import Path
+import inspect
 
 from archetypeai.common import DEFAULT_ENDPOINT, safely_extract_response_data
 from archetypeai._base import ApiBase
@@ -12,6 +13,12 @@ from archetypeai._capabilities import CapabilitiesApi
 from archetypeai._data_processing import DataProcessingApi
 from archetypeai._messaging import MessagingApi
 from archetypeai._sensors import SensorsApi
+
+def _filter_kwargs(func, kwarg_dict):
+    """Filters kwargs based on the signature of the input function."""
+    sign = set([val.name for val in inspect.signature(func).parameters.values()])
+    filtered_dict = {key: kwarg_dict[key] for key in sign.intersection(kwarg_dict.keys())}
+    return filtered_dict
 
 
 class ArchetypeAI(ApiBase):
@@ -28,5 +35,5 @@ class ArchetypeAI(ApiBase):
         self.files = FilesApi(api_key, api_endpoint)
         self.capabilities = CapabilitiesApi(api_key, api_endpoint)
         self.data_processing = DataProcessingApi(api_key, api_endpoint)
-        self.messaging = MessagingApi(api_key, api_endpoint, **kwargs)
-        self.sensors = SensorsApi(api_key, api_endpoint, **kwargs)
+        self.messaging = MessagingApi(api_key, api_endpoint, **_filter_kwargs(MessagingApi.__init__, kwargs))
+        self.sensors = SensorsApi(api_key, api_endpoint, **_filter_kwargs(SensorsApi.__init__, kwargs))
