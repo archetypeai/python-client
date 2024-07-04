4a08178a0bdac56230daf52263bc380ed227db33
Nick Gillian <nick@archetypeai.team>
2024-07-04 17:57:00 +0000

    Moved filter_kwargs to common helper.

--- src/archetypeai/common.py
+++ src/archetypeai/common.py
@@ -1,5 +1,7 @@
-import requests
 from typing import Dict
+import inspect
+
+import requests
 
 DEFAULT_ENDPOINT = "https://staging.archetypeai.dev/v0.5"
 
@@ -11,3 +13,10 @@ def safely_extract_response_data(response: requests.Response) -> Dict:
         return response_data
     except:
         return {}
+
+
+def filter_kwargs(func, kwarg_dict):
+    """Filters kwargs based on the signature of the input function."""
+    sign = set([val.name for val in inspect.signature(func).parameters.values()])
+    filtered_dict = {key: kwarg_dict[key] for key in sign.intersection(kwarg_dict.keys())}
+    return filtered_dict
