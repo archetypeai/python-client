e3a17f1fe75ce6e573e8654f05e8947956b0541a
Nick Gillian <nick@archetypeai.team>
2024-09-13 05:28:29 +0000

    Switched default API to stable endpoint.

--- src/archetypeai/_common.py
+++ src/archetypeai/_common.py
@@ -3,7 +3,7 @@ import inspect
 
 import requests
 
-DEFAULT_ENDPOINT = "https://staging.archetypeai.dev/v0.5"
+DEFAULT_ENDPOINT = "https://api.archetypeai.dev/v0.5"
 
 
 def safely_extract_response_data(response: requests.Response) -> Dict:
