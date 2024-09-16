f7a0ab783a2d41aea228649d1630720f5344b851
Nick Gillian <nick@archetypeai.team>
2024-09-16 20:41:36 +0000

    Added get message request throttling.

--- src/archetypeai/_messaging.py
+++ src/archetypeai/_messaging.py
@@ -1,5 +1,6 @@
 from typing import Any
 import json
+import time
 
 from archetypeai._base import ApiBase
 
@@ -7,9 +8,16 @@ from archetypeai._base import ApiBase
 class MessagingApi(ApiBase):
     """Main class for handling all messaging API calls."""
 
-    def __init__(self, api_key: str, api_endpoint: str, client_name: str = "python_client") -> None:
+    def __init__(
+        self,
+        api_key: str,
+        api_endpoint: str,
+        client_name: str = "python_client",
+        rate_limiter_timeout_sec: float = 0.5) -> None:
         super().__init__(api_key, api_endpoint)
         self.client_name = client_name
+        self.rate_limiter_timeout_sec = rate_limiter_timeout_sec
+        self.last_get_time = 0.0
         self.subscriber_info = []
     
     def subscribe(self, topic_ids: list[str]) -> dict:
@@ -29,6 +37,14 @@ class MessagingApi(ApiBase):
     
     def get_next_messages(self) -> list[dict]:
         messages = []
+        current_time = time.time()
+        # Throttle get requests if needed.
+        time_delta = current_time - self.last_get_time
+        if self.rate_limiter_timeout_sec > 0.0 and time_delta <= self.rate_limiter_timeout_sec:
+            sleep_time_sec = self.rate_limiter_timeout_sec - time_delta
+            logging.warning(f"Reache rate limiter, waiting {sleep_time_sec:.2f} sec")
+            time.sleep(sleep_time_sec)
+        self.last_get_time = current_time
         for subscriber_info in self.subscriber_info:
             subscriber_uid = subscriber_info["subscriber_uid"]
             api_endpoint = self._get_endpoint(self.api_endpoint, "messaging/retrieve")
