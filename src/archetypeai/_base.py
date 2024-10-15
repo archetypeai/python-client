6ac7225b0b840e1750266f2cc9c24dd6fa8b1eae
Nick Gillian <nick.gillian@archetypeai.team>
2024-10-14 18:02:30 -0700

    Generalized protocol logic.

--- src/archetypeai/_base.py
+++ src/archetypeai/_base.py
@@ -18,15 +18,13 @@ class ApiBase:
                  api_endpoint: str = DEFAULT_ENDPOINT,
                  num_retries: int = 3,
                  log_level: int = logging.INFO,
-                 log_format: str = "[%(asctime)s] %(message)s",
-                 use_ssl: bool = True
+                 log_format: str = "[%(asctime)s] %(message)s"
                  ) -> None:
         self.api_key = api_key
         self.api_endpoint = api_endpoint
         self.auth_headers = {"Authorization": f"Bearer {self.api_key}"}
         self.num_retries = num_retries
         self.valid_response_codes = (200, 201)
-        self.use_ssl = use_ssl
         logging.basicConfig(level=log_level, format=log_format, datefmt="%H:%M:%S", stream=sys.stdout)
     
     def requests_get(self, api_endpoint: str, params: dict = {}, additional_headers: dict = {}) -> dict:
@@ -64,9 +62,12 @@ class ApiBase:
                 subpath = arg
             else:
                 subpath = Path(subpath) / Path(arg)
-        http_protocol = "https://" if self.use_ssl else "http://"
-        ws_protocol = "wss://" if self.use_ssl else "ws://"
-        protocol = http_protocol if http_protocol in base_endpoint else ws_protocol
+        protocol= ""
+        potential_protocols = ("https://", "http://", "wss://", "ws://")
+        for potential_protocol in potential_protocols:
+            if base_endpoint.startswith(potential_protocol):
+                protocol = potential_protocol
+                break
         api_endpoint = protocol + str(Path(base_endpoint.replace(protocol, "")) / Path(subpath))
         api_endpoint = api_endpoint.replace("\\", "/")  # Needed for Windows style joins.
         return api_endpoint
