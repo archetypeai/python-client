4c4cb538bd31f5f153e5c1079964c9975da20ac4
Nick Gillian <nick@archetypeai.team>
2024-11-09 05:44:05 +0000

    Removed data processing api.

--- src/archetypeai/api_client.py
+++ src/archetypeai/api_client.py
@@ -1,15 +1,7 @@
-import requests
-import json
-from requests_toolbelt import MultipartEncoder
-import os
-from typing import Dict, List, Tuple
-from pathlib import Path
-
 from archetypeai.__init__ import __version__
 from archetypeai._base import ApiBase
 from archetypeai._capabilities import CapabilitiesApi
 from archetypeai._common import DEFAULT_ENDPOINT, filter_kwargs
-from archetypeai._data_processing import DataProcessingApi
 from archetypeai._files import FilesApi
 from archetypeai._messaging import MessagingApi
 from archetypeai._sensors import SensorsApi
@@ -20,7 +12,6 @@ class ArchetypeAI(ApiBase):
 
     files: FilesApi
     capabilities: CapabilitiesApi
-    data_processing: DataProcessingApi
     messaging: MessagingApi
     sensors: SensorsApi
 
@@ -33,6 +24,5 @@ class ArchetypeAI(ApiBase):
         super().__init__(api_key, api_endpoint)
         self.files = FilesApi(api_key, api_endpoint, **filter_kwargs(FilesApi.__init__, kwargs))
         self.capabilities = CapabilitiesApi(api_key, api_endpoint, **filter_kwargs(CapabilitiesApi.__init__, kwargs))
-        self.data_processing = DataProcessingApi(api_key, api_endpoint, **filter_kwargs(DataProcessingApi.__init__, kwargs))
         self.messaging = MessagingApi(api_key, api_endpoint, **filter_kwargs(MessagingApi.__init__, kwargs))
         self.sensors = SensorsApi(api_key, api_endpoint, **filter_kwargs(SensorsApi.__init__, kwargs))
