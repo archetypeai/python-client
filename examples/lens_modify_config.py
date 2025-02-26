4979d08e3ec795f0424cbb7d9c2662718774a523
Nick Gillian <nick@archetypeai.team>
2025-02-26 09:18:24 +0000

    Simplified lens modify example.

--- examples/lens_modify_config.py
+++ examples/lens_modify_config.py
@@ -1,12 +1,9 @@
-# An example that demonstrates how to modify the focus of a lens during a live stream.
+# An example that demonstrates how to fetch and modify the focus of a lens.
 # usage:
-#   python -m examples.files_api --api_key=<YOUR_API_KEY> --filename=<PATH_TO_LOCAL_VIDEO_FILE>
-#   python -m examples.lens_csv_stream --api_key=<YOUR_API_KEY> --file_id=<VIDEO_FILE_ID>
+#   python -m examples.lens_modify_config --api_key=<YOUR_API_KEY>
 import argparse
 import logging
 from pprint import pformat
-import time
-import secrets
 
 from archetypeai.api_client import ArchetypeAI
 
@@ -25,45 +22,13 @@ def main(args):
         session_id=session_id, session_endpoint=session_endpoint)
     assert is_connected
 
-    # Attach a video file reader as input to the lens.
+    # Read the current focus of the lens.
     event = {
-        "type": "input_stream.set",
-        "event_data": {
-            "stream_type": "video_file_reader",
-            "stream_config": {
-                "file_id": args.file_id,
-                "window_size": args.window_size, # How many frames per event.
-                "step_size": args.step_size,     # How many steps per window.
-            }
-        }
+        "type": "session.get",
+        "event_data": {},
     }
-    logging.info(f"Sending event: {pformat(event, indent=4, depth=2)}")
     response = client.lens.sessions.write(session_id, event)
-    logging.info(f"response: \n {pformat(response, indent=4)}")
-
-    # Attach a kafka writer as output from the lens.
-    topic_uid = secrets.token_hex(8)  # Generate a unique topic for every run.
-    topic_id = f"example_detections_{topic_uid}"
-    event = {
-        "type": "output_stream.set",
-        "event_data": {
-            "stream_type": "kafka_writer",
-            "stream_config": {
-                "topic_ids": [topic_id],
-            }
-        }
-    }
-    logging.info(f"Sending event: {pformat(event, indent=4, depth=2)}")
-    response = client.lens.sessions.write(session_id, event)
-    logging.info(f"response: \n {pformat(response, indent=4)}")
-
-    # Create a kafka consumer to read the output of the lens, which will contain
-    # the CSV rows.
-    consumer = client.kafka.create_consumer(
-        topic_id=topic_id,
-        auto_offset_reset="earliest",
-        consumer_timeout_ms=1000
-    )
+    logging.info(f"- old_focus: {response['event_data']['focus']}")
 
     # Adjust the focus of the lens.
     event = {
@@ -72,17 +37,15 @@ def main(args):
             "focus": "trucks",
         }
     }
-    logging.info(f"Sending event: {pformat(event, indent=4, depth=2)}")
     response = client.lens.sessions.write(session_id, event)
-    logging.info(f"response: \n {pformat(response, indent=4)}")
 
-    start_time = time.time()
-    while True:
-        for message in consumer:
-            logging.info(message.value)
-        if time.time() - start_time > args.max_run_time_sec:
-            break
-        time.sleep(0.5)
+    # Read the new focus of the lens.
+    event = {
+        "type": "session.get",
+        "event_data": {},
+    }
+    response = client.lens.sessions.write(session_id, event)
+    logging.info(f"- new_focus: {response['event_data']['focus']}")
 
     # Clean up the session.
     response = client.lens.sessions.destroy(session_id)
@@ -91,12 +54,8 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--file_id", required=True, type=str)
     parser.add_argument("--api_key", required=True, type=str)
     parser.add_argument("--api_endpoint", default=ArchetypeAI.get_default_endpoint(), type=str)
     parser.add_argument("--lens_id", default="lns-fd669361822b07e2-237ab3ffd79199b2", type=str)
-    parser.add_argument("--max_run_time_sec", default=10.0, type=float)
-    parser.add_argument("--window_size", default=1, type=int)
-    parser.add_argument("--step_size", default=1, type=int)
     args = parser.parse_args()
     main(args)
