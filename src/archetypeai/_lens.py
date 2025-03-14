7db7c4cca1bf537a6442b53346fd8ba2d6088518
Nick Gillian <nick@archetypeai.team>
2025-03-14 02:06:42 +0000

    Added LensSessionSocket.

--- src/archetypeai/_lens.py
+++ src/archetypeai/_lens.py
@@ -1,8 +1,64 @@
 import json
 import logging
 import websocket
+from queue import Queue
+import threading
+import time
 
 from archetypeai._base import ApiBase
+
+
+class LensSessionSocket:
+
+    heartbeat_sec: int = 30
+    run_worker: bool = True
+    read_event_queue: Queue = Queue()
+    write_event_queue: Queue = Queue()
+
+    def __init__(self, session_endpoint: str, header: dict):
+        self.worker = threading.Thread(
+            target=self._worker, args=(session_endpoint, header))
+        self.worker.start()
+
+    def __del__(self):
+        self.close()
+
+    def close(self) -> bool:
+        """Stops and closes an active socket."""
+        worker_stopped = False
+        if self.run_worker:
+            self.run_worker = False
+            self.worker.join()
+            self.worker = None
+            worker_stopped = True
+        return worker_stopped
+
+    def send_and_recv(self, event_data: dict) -> dict:
+        """Writes an event to an open session and returns the response."""
+        self.write_event_queue.put(event_data)
+        response = self.read_event_queue.get()
+        response = json.loads(response)
+        return response
+    
+    def _worker(self, session_endpoint: str, header: dict):
+        socket = websocket.create_connection(session_endpoint, header=header)
+        last_event = time.time()
+        while self.run_worker:
+            current_time = time.time()
+            if not self.write_event_queue.empty():
+                # Send the event to the server.
+                event_data = self.write_event_queue.get()
+                last_event = time.time()
+                socket.send_binary(json.dumps(event_data).encode())
+                # Read back the response.
+                event_data = socket.recv()
+                self.read_event_queue.put(event_data)
+            else:
+                time.sleep(0.1)
+            # Send a periodic heartbeat to keep the connection alive.
+            if current_time - last_event >= self.heartbeat_sec:
+                last_event = time.time()
+                socket.ping()
 
 
 class SessionsApi(ApiBase):
@@ -12,6 +68,11 @@ class SessionsApi(ApiBase):
 
     def __init__(self, api_key: str, api_endpoint: str) -> None:
         super().__init__(api_key, api_endpoint)
+
+    def __del__(self):
+        for session_id in self.subscriber_cache:
+            self.subscriber_cache[session_id].close()
+        self.subscriber_cache = {}
 
     def get_info(self) -> dict:
         """Gets the high-level info for all lens sessions across your org."""
@@ -44,8 +105,7 @@ class SessionsApi(ApiBase):
 
     def connect(self, session_id: str, session_endpoint: str) -> bool:
         try:
-            socket = websocket.create_connection(
-                session_endpoint, header={"Authorization":f"Bearer {self.api_key}"})
+            socket = LensSessionSocket(session_endpoint, {"Authorization":f"Bearer {self.api_key}"})
             self.subscriber_cache[session_id] = socket
         except Exception as exception:
             logging.exception(f"Failed to connect to session at {session_endpoint}")
@@ -63,10 +123,7 @@ class SessionsApi(ApiBase):
     def write(self, session_id: str, event_data: dict) -> dict:
         """Writes an event to an open session and returns the response."""
         assert session_id in self.subscriber_cache, f"Unknown session ID {session_id}"
-        socket = self.subscriber_cache[session_id]
-        socket.send_binary(json.dumps(event_data).encode())
-        response = socket.recv()
-        response = json.loads(response)
+        response = self.subscriber_cache[session_id].send_and_recv(event_data)
         return response
 
 
