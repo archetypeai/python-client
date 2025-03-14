8213349f431ba2af179f01f58dae3a8d714b1723
Nick Gillian <nick@archetypeai.team>
2025-03-14 02:09:20 +0000

    Updated version to 25.03.13.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="archetypeai",
-    version="25.02.25",
+    version="25.03.13",
     author="Archetype AI",
     url="https://github.com/archetypeai/python-client",
     description="The official python client for the Archetype AI API.",
