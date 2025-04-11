79c5b2eb2190f46c6b7c362e6ec08afa6c9ebc16
Nick Gillian <nick@archetypeai.team>
2025-04-11 23:26:48 +0000

    Syncing local change.

--- src/archetypeai/_lens.py
+++ src/archetypeai/_lens.py
@@ -14,7 +14,7 @@ class LensSessionSocket:
     """Manages websocket connections for each lens session."""
 
     heartbeat_sec: int = 30
-    run_worker: bool = True
+    run_worker: bool = False
     read_event_queue: Queue = Queue()
     write_event_queue: Queue = Queue()
 
@@ -44,14 +44,25 @@ class LensSessionSocket:
         return response
     
     def _worker(self, session_endpoint: str, header: dict):
+        self.run_worker = True
+        while self.run_worker:
+            try:
+                self.run_worker = self._run_worker_loop(session_endpoint, header)
+            except Exception as exception:
+                logging.exception("Failed to run socket loop - restarting...")
+
+    def _run_worker_loop(self, session_endpoint: str, header: dict) -> bool:
         socket = websocket.create_connection(session_endpoint, header=header)
+        start_time = time.time()
         last_event = time.time()
         while self.run_worker:
             current_time = time.time()
+            run_time = current_time - start_time
             if not self.write_event_queue.empty():
+                last_event = time.time()
                 # Send the event to the server.
                 event_data = self.write_event_queue.get()
-                last_event = time.time()
+                logging.debug(f"[{run_time:.2f}] Sending event w/ type: {event_data['type']}...")
                 socket.send_binary(json.dumps(event_data).encode())
                 # Read back the response.
                 event_data = socket.recv()
@@ -60,8 +71,8 @@ class LensSessionSocket:
                 time.sleep(0.1)
             # Send a periodic heartbeat to keep the connection alive.
             if current_time - last_event >= self.heartbeat_sec:
-                last_event = time.time()
-                socket.ping()
+                self.write_event_queue.put({"type": "session.heartbeat"})
+        return self.run_worker
 
 
 class SessionsApi(ApiBase):
