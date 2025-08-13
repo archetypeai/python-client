2fa100639fcf5d46b8ea3251a0d887e21aec3cb9
Nick Gillian <nick@archetypeai.team>
2025-08-13 15:52:06 +0000

    Updated quickstart to simple video analysis lens.

--- examples/quickstart.py
+++ examples/quickstart.py
@@ -1,20 +1,45 @@
-# An example that demonstrates changing the focus of a lens
+# An example that demonstrates how to hook up a video and stream it to a lens.
 # usage:
-#   python quickstart.py --api_key=<YOUR_API_KEY> --lens_id=<LENS_ID> --filename=<FILENAME>
+#   python -m examples.quickstart --api_key=<YOUR_API_KEY> --filename=<VIDEO_FILE_ID>
 import argparse
 import logging
-import sys
+
+import yaml
 
 from archetypeai.api_client import ArchetypeAI
-from archetypeai.utils import base64_encode
+from archetypeai.utils import pformat
+
 
 def main(args):
     # Create a new client using your unique API key.
     client = ArchetypeAI(args.api_key, api_endpoint=args.api_endpoint)
 
+    # Define a custom lens.
+    lens_config = f"""
+       lens_name: Custom Activity Monitor
+       lens_config:
+        model_parameters:
+            model_version: Newton::c2_1_250408d4362cc9
+            instruction: {args.instruction}
+            focus: {args.focus}
+            max_new_tokens: {args.max_new_tokens}
+            camera_buffer_size: {args.camera_buffer_size}
+            camera_buffer_step_size: {args.camera_buffer_step_size}
+    """
+    lens_config = yaml.safe_load(lens_config)
+    logging.info(f"Lens config:\n{pformat(lens_config)}")
+
+    # Register the custom lens with the Archetype AI platform.
+    lens_metadata = client.lens.register(lens_config)
+    lens_id = lens_metadata.get("lens_id", None)
+    assert lens_id, f"Missing lens_id: {lens_metadata}"
+
     # Create and run a new session, using the session function below.
     client.lens.create_and_run_session(
-        args.lens_id, session_fn, auto_destroy=True, client=client, args=args)
+        lens_id, session_fn, auto_destroy=True, client=client, args=args)
+
+    # Delete the custom lens to clean things up.
+    client.lens.delete(lens_id)
 
 def session_fn(
         session_id: str,
@@ -23,97 +48,79 @@ def session_fn(
         args: dict
     ) -> None:
     """Main function to run the logic of a custom lens session."""
-    # Extract the default values from the lens.
-    lens_config = read_default_lens_config(client, args.lens_id)
 
-    # Query the status of the session.
-    log_heading("Get session status")
-    event = {"type": "session.status"}
-    response = client.lens.sessions.process_event(session_id, event)
-    logging.debug(response)
+    # Upload the video file to the archetype platform.
+    response_data = client.files.local.upload(args.filename)
+    assert "file_id" in response_data, response_data
+    file_id = response_data["file_id"]
 
-    # Validate the session.
-    log_heading("Validate the session")
-    event = {"type": "session.validate"}
-    response = client.lens.sessions.process_event(session_id, event)
-    logging.debug(f"{response}")
-    if "message" in response:
-        logging.error(response["message"])
-        return
-    elif "event_data" in response and response["event_data"]["is_valid"] == False:
-        logging.error(response["event_data"]["error_messages"])
-        return
-
-    # Load the input image and convert it to a base64 encoding.
-    try:
-        base64_img = base64_encode(args.filename)
-    except FileNotFoundError as err:
-        logging.exception(f"Failed to read file {args.filename}")
-        return
-
-    instruction = ""
-    if "instruction" in lens_config["model_parameters"]:
-        instruction = lens_config["model_parameters"]["instruction"]
-
-    # Create a model query event and send this directly to the lens.
+    # Adjust the focus of the lens.
     event = {
-        "type": "model.query",
+        "type": "session.modify",
         "event_data": {
-            "model_version": lens_config["model_parameters"]["model_version"],
-            "template_name": lens_config["model_parameters"]["template_name"],
-            "instruction": instruction,
+            "instruction": args.instruction,
             "focus": args.focus,
             "max_new_tokens": args.max_new_tokens,
-            "data": [{
-                    "type": "base64_img",
-                    "base64_img": base64_img,
-            }  ],
-            "sensor_metadata": {}
+            "camera_buffer_size": args.camera_buffer_size,
+            "camera_buffer_step_size": args.camera_buffer_step_size,
         }
     }
-    log_heading("Running prediction")
     response = client.lens.sessions.process_event(session_id, event)
-    logging.debug(response)
+    assert response["event_data"]["is_valid"], response
 
-    # Show the description of the image
-    log_heading("Response")
-    if (response["event_data"] is not None
-          and "response" in response["event_data"]):
-        logging.info(response["event_data"]["response"])
-    else:
-        logging.info(response["event_data"])
+    # Attach an output stream using the ServerSideEvents protocol so we can recieve real-time outputs from the lens.
+    event = {
+        "type": "output_stream.set",
+        "event_data": {
+            "stream_type": "server_side_events_writer",
+            "stream_config": {},
+        }
+    }
+    response = client.lens.sessions.process_event(session_id, event)
+    assert response["event_data"]["is_valid"], response
 
-def read_default_lens_config(client: ArchetypeAI, lens_id: str) -> dict:
-    """Queries the metadata for a specific lens and returns the lens config."""
-    log_heading("Get lens metadata")
-    try:
-        lens_metadata = client.lens.get_metadata(lens_id=lens_id)
-    except ValueError as err:
-        logging.exception(f"Failed to fetch metadata for lens {lens_id}")
-        exit()
-    assert len(lens_metadata) == 1, lens_metadata
-    lens_name = lens_metadata[0]["lens_name"]
-    lens_config = lens_metadata[0]["lens_config"]
-    lens_model = lens_config["model_parameters"]["model_version"]
-    logging.info(f"Lens ID   : {lens_id} ")
-    logging.info(f"Lens name : {lens_name}")
-    logging.info(f"Lens model: {lens_model}")
-    return lens_config
+    # Lastly, attach the an input stream to start recording data from a video file reader as input to the lens.
+    event = {
+        "type": "input_stream.set",
+        "event_data": {
+            "stream_type": "video_file_reader",
+            "stream_config": {
+                "file_id": file_id,
+                "step_size": args.step_size,     # How granular to step through the video.
+            }
+        }
+    }
+    response = client.lens.sessions.process_event(session_id, event)
+    assert response["event_data"]["is_valid"], response
 
-def log_heading(heading: str) -> None:
-    logging.info(f"\x1b[35m--- {heading} ---\x1b[32m")
+    # Create a SSE reader to read the output of the lens.
+    sse_reader = client.lens.sessions.create_sse_consumer(
+        session_id, max_read_time_sec=args.max_run_time_sec)
+
+    # Read events from the SSE stream until either the last message is
+    # received or the max read time has been reached.
+    for event in sse_reader.read(block=True):
+        logging.info(event)
+
+    # Close any active reader.
+    sse_reader.close()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--api_key", required=True, type=str, help="Set your Archetype API key")
-    parser.add_argument("--api_endpoint", default=ArchetypeAI.get_default_endpoint(), type=str, help="Set the API endpoint")
-    parser.add_argument("--lens_id", default="lns-fd669361822b07e2-bc608aa3fdf8b4f9", type=str, help="Set the lens ID")
-    parser.add_argument("--filename", required=True, type=str, help="Set the local filename")
-    parser.add_argument("--focus", default="Describe the image.", type=str, help="Set the focus of the lens")
-    parser.add_argument("--max_new_tokens", default=256, type=int, help="set the max new")
-    parser.add_argument("--debug", action="store_true", help="Show debug information")
+    parser.add_argument("--filename", required=True, type=str)
+    parser.add_argument("--api_key", required=True, type=str)
+    parser.add_argument("--api_endpoint", default=ArchetypeAI.get_default_endpoint(), type=str)
+    parser.add_argument("--instruction", default="Analyze the video with the following focus.", type=str)
+    parser.add_argument("--focus", default="Describe the actions in the video.", type=str)
+    parser.add_argument("--max_run_time_sec", default=10.0, type=float)
+    parser.add_argument("--step_size", default=30, type=int)
+    parser.add_argument("--camera_buffer_size", default=5, type=int)
+    parser.add_argument("--camera_buffer_step_size", default=5, type=int)
+    parser.add_argument("--max_new_tokens", default=256, type=int)
     args = parser.parse_args()
-    log_level = logging.DEBUG if args.debug else logging.INFO
-    log_formatter = "%(asctime)s %(levelname)s %(message)s"
-    logging.basicConfig(level=log_level, stream=sys.stdout, format=log_formatter, datefmt="%H:%M:%S")
+
+    # Validate the input.
+    assert args.filename.endswith(".mp4"), "Enter an .mp4 video file"
+
     main(args)
