cb3ddc14a3620d7bca711da88e9d2510966581e8
Nick Gillian <nick@archetypeai.team>
2025-12-17 22:46:20 +0000

    Direct import the ApiError.

--- src/archetypeai/__init__.py
+++ src/archetypeai/__init__.py
@@ -1,5 +1,6 @@
-from .api_client import ArchetypeAI, ApiError
+from .api_client import ArchetypeAI
 from .utils import pformat, configure_logging
+from ._errors import ApiError
 
 __all__ = ["ArchetypeAI", "ApiError", "pformat", "configure_logging"]
 __version__ = ArchetypeAI.get_version()
