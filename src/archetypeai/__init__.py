016e60be14a2cff9f631b3df2bf3466ad7fb7e77
Nick Gillian <nick@archetypeai.team>
2025-12-17 22:44:56 +0000

    Added ApiError

--- src/archetypeai/__init__.py
+++ src/archetypeai/__init__.py
@@ -1,5 +1,5 @@
-from .api_client import ArchetypeAI
+from .api_client import ArchetypeAI, ApiError
 from .utils import pformat, configure_logging
 
-__all__ = ["ArchetypeAI", "pformat", "configure_logging"]
+__all__ = ["ArchetypeAI", "ApiError", "pformat", "configure_logging"]
 __version__ = ArchetypeAI.get_version()
