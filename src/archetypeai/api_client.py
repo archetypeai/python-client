34889927efdce3ce88d2321f5f2ee253f9336bfb
Nick Gillian <nick@archetypeai.team>
2026-02-24 07:33:03 +0000

    Updated version to 26.02.23.0

--- src/archetypeai/api_client.py
+++ src/archetypeai/api_client.py
@@ -10,7 +10,7 @@ from archetypeai._sensors import SensorsApi
 from archetypeai._lens import LensApi
 from archetypeai._kafka_client import KafkaApi
 
-_VERSION = "25.12.17.6"
+_VERSION = "26.02.23.0"
 
 
 class ArchetypeAI(ApiBase):
